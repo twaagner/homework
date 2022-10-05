ca6e8473561a6ef26a0e448d11c0e939d24acd5f
Travis Waagner <twaagner@gmail.com>
2022-10-04 22:08:11 -0400

    lesson 7 exercise revisit

--- Lesson 07/03_functions-codealong/starter_code/script.js
+++ Lesson 07/03_functions-codealong/starter_code/script.js
@@ -10,11 +10,26 @@ console.log(addFour(4));
 console.log(addFour(-1));
 console.log(addFour(37));
 
+
+function addFour(myValue) {
+    let outputNumber = myValue + 4;
+    return outputNumber;
+}
 // 1. Square of a number
 
 // Create a function called `squared` that takes one argument
 // The function will return the argument times itself (the square of the number)
 
+
+function squaredNumber(inPut){
+   outPut = inPut * inPut;
+   return outPut;
+}
+console.log(squaredNumber(4))
+console.log(squaredNumber(7))
+
+
+// tuesday revisit //
 function squared(inputNumber) {
     let outputNumber = inputNumber * inputNumber;
     return outputNumber; 
@@ -33,23 +48,47 @@ console.log(squared(-3));
 
 function stringLength(yarn){
     return yarn.length;
-  
-}
+  }
 
 console.log(stringLength("testing"))
 console.log(stringLength("This is a longer string."))
+
+// tuesday revisit //
+function stringLength(yarn){
+    return yarn.length;
+}
+
+console.log(stringLength('youCanNotHaveThis'))
+console.log(stringLength('umpqua'))
 
 // 3. Add two numbers together
 
 // Create a function called addNumbers
 // Takes two arguments and returns the sum of those arguments
 
-// console.log(addNumbers(4, 2));
-// console.log(addNumbers(6, 3));
+// tuesday revisit//
+
+function addNumbers(t,w){
+    return t + w;
+}
+
+console.log(addNumbers(50, 1))
+console.log(addNumbers(-3, 4))
+console.log(addNumbers(4, 2));
+console.log(addNumbers(6, 3));
 
 // 4. Create a function called sayHello
 // Takes one argument
 // Returns a string that is the combination of "Hello, " + the argument passed into the function
 // So sayHello("Paul") would return "Hello, Paul";
 
+// tuesday revisit //
+
+function sayHello(name){
+    return "Hello, " + name;
+}
+
+console.log(sayHello('Travis'))
+console.log(sayHello('Mr Milo Sir'))
+console.log(sayHello('sensei miagi'))
 // console.log(sayHello("Paul"));
