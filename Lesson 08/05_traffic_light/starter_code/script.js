7aefbbfc8bde5f6cbaad2d5f124c0a40697a46ed
twaagner <twaagner@gmail.com>
2022-10-10 22:13:47 -0400

    added lesson 9 & worked on lesson 9

--- Lesson 08/05_traffic_light/starter_code/script.js
+++ Lesson 08/05_traffic_light/starter_code/script.js
@@ -1,2 +1,39 @@
 // Try making the traffic light work - Think through the problem BEFORE you code!
 // You'll use addEventListener and getElementById if you're doing it right...
+
+// function that turns off all of the lights
+
+
+// function that turns off all the lights
+function turnOff(){
+    document.querySelector('#stopLight').classList.remove('stop');
+    document.querySelector('#slowLight').classList.remove('slow');
+    document.querySelector('#goLight').classList.remove('go');
+}
+
+// Go function
+function go(){
+    turnOff();
+    document.getElementById('goLight').classList.add('go');
+    console.log("it works!")
+}
+
+
+// Slow function
+function slow(){
+    turnOff();
+    document.getElementById('slowLight').classList.add('slow');
+}
+
+
+// Stop Function
+function stop(){
+    turnOff();
+    document.getElementById('stopLight').classList.add('stop');
+}
+
+// Bind to clicks
+document.getElementById('stopButton').addEventListener('click',stop);
+document.querySelector('#slowButton').addEventListener('click',slow);
+document.querySelector("#goButton").addEventListener('click',go);
+
