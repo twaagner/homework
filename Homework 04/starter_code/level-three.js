eae11d24fbef53f3284064b353f0835aea79bc75
Travis Waagner <twaagner@gmail.com>
2022-10-13 23:02:57 -0400

    hw 4 complete

--- Homework 04/starter_code/level-three.js
+++ Homework 04/starter_code/level-three.js
@@ -6,12 +6,29 @@ let score = 0;
 let arenaEl = document.querySelector('.js-arena');
 let scoreEl = document.querySelector('.js-score');
 
-// add 10 pts when any ball is clicked///
+// retrieve data increments and then add 10 pts when any ball is clicked///
+
+///////////////  retrieving data increments //////////////////////
+
+      // let score = getAttribute('data-increment');
+
+      // score += parseInt(scoreAdd)
+
+
 
 // arenaEl.addEventListener('click',function(clickBall){
-document.querySelector('.js-arena').addEventListener('click', function(e) {
-    if ( e.target.classList.contains('js-ball') ) {
-      score += 10;
+document.querySelector('.js-arena').addEventListener('click', function(clickBall) {
+    if ( clickBall.target.classList.contains('js-ball') ) {console.log(clickBall.target.getAttribute('data-increment'))
+
+    
+    
+    // parseInt() - take string and turn to number // 
+
+    // += adds value of the right operator to the variable, in our case, the score
+
+
+      score += parseInt(clickBall.target.dataset.increment);
+      // score += 5, 10, or 20 depending on which balll is clicked
       if ( score < 100 ) {
         scoreEl.innerText = score;
       } else {
@@ -20,9 +37,9 @@ document.querySelector('.js-arena').addEventListener('click', function(e) {
     }
   })
 
+function declareWinner(){
+    document.body.classList.add('level-winner');
+  }
 
-//   retrieving data increments //
 
-let score = getAttribute('data-increment');
 
-score += parseInt(scoreAdd)
