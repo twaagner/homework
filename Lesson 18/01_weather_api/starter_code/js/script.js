6cffd43635fc5e230cbf25b8f9c709f81b394ea9
Travis Waagner <twaagner@gmail.com>
2022-11-09 21:24:09 -0500

    exercise complete

--- Lesson 18/01_weather_api/starter_code/js/script.js
+++ Lesson 18/01_weather_api/starter_code/js/script.js
@@ -1,4 +1,3 @@
-
 // ToDo
 // When the user submits the search form, make an API request to open weather api's search endpoint
 // https://api.openweathermap.org/data/2.5/weather?q=CITY&APPID=KEY
@@ -7,54 +6,82 @@
 // Display the results in the #weather-results div
 // Each new search should erase the previous results
 
-const myApiKey = "730eecb2c92a56917eac9bf8c07df80b";
+const myApiKey = "870b1b10b58578f725b70b13aff6c357";
 
 async function handleWeatherRequest(e) {
   e.preventDefault();
-  
+
   // get user input value from textbox
-  let city = document.querySelector('input [name=city]').value;
+  let city = document.querySelector('input[name="city"]').value;
 
   // build the url address with YOUR personal API key and the users input city
   const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${myApiKey}`;
-  // have to use tic marks to imbed with curly brackets
-  
+
   const apiURLAlternative = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + myApiKey;
 
   // make an API call using fetch() and capture the response in a variable
-  const response = await fetch(apiURL);
-  // ***fetch input url into browser
  
+  const response = await fetch(apiURL);
+
   // convert the response to a readable json format with .json()
 
-  const convert = await response.json();
+  console.log(response);
+
+  const blueberry = await response.json();
   
   // print your data to the console to see its format, dont forget to delete later
-  console.log(convert);
   
+  console.log(blueberry);
+
   // Write a separate function to convert the temps from the default units (Kelvin) to a more readable F or C
-
   
-  // build the icon src
+  const currentTemp = convertKtoF(blueberry.main.temp);
+  const feelsLikeTemp = convertKtoF(blueberry.main.feels_like);
+  const humidity = blueberry.main.humidity;
+  const pressure = numberWithCommas(blueberry.main.pressure);
+  const highTemp = convertKtoF(blueberry.main.temp_max);
+  const lowTemp = convertKtoF(blueberry.main.temp_min);
 
+  const weatherResults = `<li>The temperature in ${blueberry.name} is currently ${currentTemp}&#8457;</li>
+  <li>Feels Like: ${feelsLikeTemp}&#8457;</li>
+  <li>Humidity: ${humidity}%</li>
+  <li>Pressure: ${pressure}</li>
+  <li>High: ${highTemp}&#8457;</li>
+  <li>Low: ` + lowTemp + `&#8457;</li>`;
+
+
+  // build the icon src
   
   // create icon img tag
-
   
   // add icon url as src
   
-
   // clear out previous icon img
   
-
   // print all results to dom
-  
+  document.getElementById('weather-results').innerHTML = weatherResults;
+
 
   // append icon img to dom
   
+}
 
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function convertKtoF(kelvinTemp){
+  let tempF = (kelvinTemp - 273.15) * 1.8 + 32;
+
+  // Round
+  tempF = Math.round(tempF);
+
+  return tempF;
 }
 
 
 // Attach an event listener to the submit button
-document.querySelector
+document.querySelector('#weather-search').addEventListener('submit',handleWeatherRequest);
+
+
